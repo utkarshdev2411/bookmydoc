20ea4817d58b0c1c0142c15a3e3d58198b2326b7
Shikha Gupta <shikha912513@gmail.com>
2024-11-03 14:39:12 +0530

    modified backend

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -272,23 +272,6 @@ exports.getAppointmentsByUserId = async (req, res) => {
 };
 
 
-exports.getAppointmentsByUserId = async (req, res) => {
-  try {
-    const appointments = await Appointment.find({ userId: req.body.userId });
-    res.status(200).send({
-      message: "Appointments fetched successfully",
-      success: true,
-      data: appointments,
-    });
-  } catch (error) {
-    console.log(error);
-    return res.status(500).send({
-      message: "Error while getting appointment details",
-      success: false,
-    });
-  }
-};
-
 
 
 
